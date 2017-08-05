731b7eb617c8fdb2a7bba020a1dc8f2a459e110f
Thierry Moudiki <thierry.moudiki@gmail.com>
2017-08-05 13:21:13 +0200

    module mixin in shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -7,4 +7,31 @@ module Shout
     words + "!!!" + " :)"
   end
 end
-= end
+
+# Test 
+Shout.yell_angrily("%+*@?")
+Shout.yell_happily("Heyyy")
+=end
+
+module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+  def yell_happily(words)
+    words + "!!!" + " :)"
+  end
+end
+
+class People1
+	include Shout
+end
+
+class People2
+	include Shout
+end
+
+person1 = People1.new
+person1.yell_happily("Yeaah")
+
+person2 = People2.new
+person2.yell_angrily("Nooo")
