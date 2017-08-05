0c9ebd0446062c461a4c64b6f4f071de9c7a5bd2
Thierry Moudiki <thierry.moudiki@gmail.com>
2017-08-05 10:57:19 +0200

    add new methods to class Puppy

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,29 @@
+
 class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(number_of_times)
+    number_of_times = number_of_times.to_i
+    number_of_times.times do
+      puts "Woof!"
+    end 
+  end
+  
+  def roll_over
+    puts "*rolls over*"
+  end
+  
+  def dog_years(number_of_years)
+    return number_of_years*9
+  end   
+  
+  def run(number_of_meters)
+    puts "I ran #{number_of_meters} meters!"
+  end   
 
 end
 
@@ -11,3 +31,15 @@ end
 
 toto = Puppy.new
 toto.fetch("ball")
+
+# 2 - Add a speak method that takes an integer, and then prints "Woof!" that many times.
+toto.speak(5)
+
+# 3 - Add a roll_over method that just prints "*rolls over*".
+toto.roll_over
+
+# 4 - Add a dog_years method that takes an integer (of human years) and converts that number to dog years, returning a new integer.
+toto.dog_years(3)
+
+# 5 - Add one more trick -- whichever one you'd like.
+toto.run(100)
