803487869fa330b384de6ca4f27bf126e4148caa
Thierry Moudiki <thierry.moudiki@gmail.com>
2017-08-12 20:20:24 +0200

    finish release 0

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,17 @@
+console.log("Release 0 \n");
 var array_of_colors = ["blue", "green", "red"];
 var array_of_names = ["gmlqme", "mvpalf", "drpfl"];
+
+console.log("Before: ");
+console.log("\n");
+console.log(array_of_colors);
+console.log(array_of_names);
+console.log("\n");
+
+array_of_colors.push("yellow");
+array_of_names.push("okvla");
+
+console.log("After: ");
+console.log("\n");
+console.log(array_of_colors);
+console.log(array_of_names);
